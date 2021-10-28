95b92006eb217555808ed6751b7bb2e42fded2d8
daiaventureira <daiane.souzas@icloud.com>
2021-10-28 17:59:17 -0400

    popula banco de dados com login profile e aplicação

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(email: 'user@a.com.br', password: '123456')
+project = Project.create!(title: "Test", skills: "testing", description: "description", price_per_hour: '230', deadline: DateTime.tomorrow, user: user)
+professional = Professional.create!(email: "pro@pro2.com.br", password: "123456")
+b_date = 19.years.ago
+Profile.create(full_name: 'Juline Iza', social_name: "Iza", description: "hello", birth_date: b_date, educational_background: 'arts', experience: '2', professional: professional)
+ProjectApplication.create!(introduction: 'Hello', professional: professional, project: project)    
